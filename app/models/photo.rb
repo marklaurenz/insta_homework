1643cdb6113614db361d98c7d0a8e99e910769a0
Mark Laurenz <m-laurenz@kellogg.northwestern.edu>
2014-05-29 21:13:43 -0500

    Upload 2

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,4 +3,6 @@ class Photo < ActiveRecord::Base
   belongs_to :user
   has_many :favorites
   has_many :fans, :through => :favorites, :source => :user
+  validates :image_url, :presence => true, :uniqueness => { :scope => :caption }
+  validates :user_id, :presence => true
 end
